3536163fb1c821d85b9bc67cd5b331e8c6b79d1c
glebishead <glebaskayumov@yandex.ru>
2023-03-28 20:18:26 +0200

    Added "create_test" file and web

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ app = Flask(__name__)
 
 @app.route('/wellcome')
 def wellcome_page():
-    return render_template('wellcome.html', name='Jerry')
+    return render_template('wellcome.html')
 
 
 @app.route('/')
 @app.route('/index')
 def main_page():
-    return render_template('main.html', name='Jerry')
+    return render_template('main.html')
+
+
+@app.route('/create_test')
+def create_test_page():
+    return render_template('create_test.html')
 
 
 if __name__ == '__main__':
