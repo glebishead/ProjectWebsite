f7144ec65340b17e2cbbed1e273b10cd34e3f509
glebishead <glebaskayumov@yandex.ru>
2023-03-30 19:15:52 +0200

    Started with sqlalchemy and POST method. Still fixing getting response data

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, request
+from data import db_session
+
 
 app = Flask(__name__)
+with open('data/keys.json', 'r', encoding='utf-8') as file:
+    data = json.loads(file.read())
+    app.config['SECRET_KEY'] = data['secret_key']
 
 
-@app.route('/wellcome')
+def main():
+    try:
+        db_session.global_init("db/blogs.db")
+        app.run(port=8080, host='127.0.0.1', debug=True)
+    except Exception as e:
+        print(e)
+
+
+@app.route('/register', methods=['GET', 'POST'])
 def wellcome_page():
-    return render_template('wellcome.html')
+    if request.method == 'POST':
+        data = request.data
+        print(data)
+        
+        # перенаправить на другую страницу
+    return render_template('register.html')
 
 
 @app.route('/')
@@ -20,7 +39,4 @@ def create_test_page():
 
 
 if __name__ == '__main__':
-    try:
-        app.run(port=8080, host='127.0.0.1')
-    except Exception as e:
-        print(e)
+    main()
